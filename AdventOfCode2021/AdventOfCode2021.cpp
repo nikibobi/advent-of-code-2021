a86ec91414262bb337dddc4910f72e83d284c58a
Borislav Kosharov <bosakmaw@gmail.com>
2021-12-08 00:25:52 +0200

    Add project files.

---
+++ AdventOfCode2021/AdventOfCode2021.cpp
@@ -0,0 +1,12 @@
+﻿// AdventOfCode2021.cpp : Defines the entry point for the application.
+//
+
+#include "AdventOfCode2021.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
