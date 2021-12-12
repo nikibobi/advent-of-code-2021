f3a158c33f0e76e47fcdd14e6abc0a8907c6c407
Borislav Kosharov <bosakmaw@gmail.com>
2021-12-12 02:58:57 +0200

    add day 03 part 2 solution

--- AdventOfCode2021/day03.cpp
+++ AdventOfCode2021/day03.cpp
@@ -31,6 +31,26 @@ bitset<N> part1(const vector<bitset<N>>& numbers, bit_criteria_t bit_criteria)
 	return result;
 }
 
+bitset<N> part2(vector<bitset<N>> numbers, bit_criteria_t bit_criteria)
+{
+	for (int i = N - 1; i >= 0; i--)
+	{
+		int ones = count_if(numbers, [i](const bitset<N>& n) { return n.test(i); });
+		int zeros = count_if(numbers, [i](const bitset<N>& n) { return !n.test(i); });
+
+		bool test = bit_criteria(zeros, ones);
+
+		auto [begin, end] = remove_if(numbers, [i, test](const auto& number) { return number.test(i) != test; });
+		numbers.erase(begin, end);
+
+		if (numbers.size() == 1)
+		{
+			break;
+		}
+	}
+	return numbers[0];
+}
+
 int main()
 {
 	vector<bitset<N>> numbers;
@@ -46,6 +66,12 @@ int main()
 	bitset<N> epsilon_rate{part1(numbers, zeros_more)};
 
 	result = gamma_rate.to_ulong() * epsilon_rate.to_ulong();
+	cout << result << endl;
+
+	bitset<N> oxygen_rate{part2(numbers, ones_more)};
+	bitset<N> co2_rate{part2(numbers, zeros_more)};
+
+	result = oxygen_rate.to_ulong() * co2_rate.to_ulong();
 	cout << result << endl;
 
 	return 0;
